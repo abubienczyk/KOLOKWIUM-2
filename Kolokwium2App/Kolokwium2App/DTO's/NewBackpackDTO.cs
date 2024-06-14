2d27843a1410a460ec59f01007e425374d69b527
abubienczyk <olabubienczyk@gmail.com>
2024-06-14 15:14:04 +0200

    final

--- Kolokwium2App/Kolokwium2App/DTO's/NewBackpackDTO.cs
+++ Kolokwium2App/Kolokwium2App/DTO's/NewBackpackDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Kolokwium2App.DTO_s;
 
 public class NewBackpackDTO
 {
+    [Required]
+    [Range(1, int.MaxValue)]
     public int Amount { get; set; }
+    [Required]
     public int ItemId { get; set; }
+    [Required]
     public int CharacterId { get; set; }
 }
